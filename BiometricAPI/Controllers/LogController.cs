1d26c6ef4cea3c8a4459dcafb0f313bacd01a301
Avinash Varma <avinashvarma@live.com>
2019-10-28 15:51:59 +0800

    Add more ways to troubleshoot the log.

---
+++ BiometricAPI/Controllers/LogController.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace BiometricAPI.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class LogController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            HttpResponseMessage httpResponseMessage;
+            var directory = @"D:\local\Temp\Logs\";
+            var files = Directory.GetFiles(directory, "*.log");
+            httpResponseMessage = Request.CreateResponse(HttpStatusCode.OK, files.ToList());
+            return httpResponseMessage;
+        }
+    }
+}
